2e86c11ada50a995e04c17fc6dfe2565dffc2d18
= <=>
2025-03-23 23:46:51 +0100

    Dockerfile, made changes for hosting

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def embed(embed_id):
     return render_template('embed.html', embed=embed_data)
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(port=5001, debug=False, host='0.0.0.0')
